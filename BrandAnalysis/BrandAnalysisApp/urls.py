b93b57b4f2073d7632e6a65ff0e1aa40ef13ecad
Vishal <vishalkalola196@icloud.com>
2020-05-09 01:23:38 +0200

    - Added Requirement text file

--- BrandAnalysis/BrandAnalysisApp/urls.py
+++ BrandAnalysis/BrandAnalysisApp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('uploadImage',views.uploadImage,name='uploadImage'),
     path('contactus',views.contactus,name='contactus'),
     path('forgotpassword',views.forgotpassword,name='forgotpassword'),
-    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    path('^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate')
 ]
