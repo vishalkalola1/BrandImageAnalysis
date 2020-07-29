0a744500de342bbde317c1840138229cf6f36837
Vishal <vishalkalola196@icloud.com>
2020-07-29 17:56:01 +0200

    - Procfile

--- BrandAnalysis/BrandAnalysis/settings.py
+++ BrandAnalysis/BrandAnalysis/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'Templates/')
+            os.path.join(BASE_DIR,'BrandAnalysisApp/Templates/')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
