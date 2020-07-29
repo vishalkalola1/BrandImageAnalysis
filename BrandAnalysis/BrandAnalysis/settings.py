b77871d40e876d4e5d2cfb15d5701a6bfef2a12e
Vishal <vishalkalola196@icloud.com>
2020-07-29 12:11:04 +0200

    - commit static

--- BrandAnalysis/BrandAnalysis/settings.py
+++ BrandAnalysis/BrandAnalysis/settings.py
@@ -137,10 +137,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
