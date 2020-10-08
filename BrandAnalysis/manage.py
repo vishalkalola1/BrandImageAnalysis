b76b31ce5ae48e8539656b13b3b2047bf7c4bcfa
vkalola <vkalola@hexaglobe.com>
2020-10-08 18:13:13 +0200

    en

--- BrandAnalysis/manage.py
+++ BrandAnalysis/manage.py
@@ -7,6 +7,7 @@ def main():
 
     try:
         from django.core.management import execute_from_command_line
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrandAnalysis.settings')
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
